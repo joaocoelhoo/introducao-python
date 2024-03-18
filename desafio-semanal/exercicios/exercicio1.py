289ff9fd18893b4271ab68105c8c0fb758a1334f
Joao Coelho <joaopaulomelocoelho@gmail.com>
2024-03-18 10:30:23 -0300

    exercicio 1 - listas duplicatas

--- desafio-semanal/exercicios/exercicio1.py
+++ desafio-semanal/exercicios/exercicio1.py
@@ -5,3 +5,11 @@
 # - Você pode utilizar conjuntos (sets) para remover duplicatas, já que conjuntos não permitem elementos duplicados.
 # - Caso queira, converta a lista para um conjunto. Em seguida, converta o conjunto de volta para uma lista.
 # - Imprima a lista resultante.
+
+def remove_duplicatas(lista):
+    lista_sem_duplicatas = list(set(lista))
+    print(lista_sem_duplicatas)
+
+
+lista_com_duplicatas = [9, 9, 5, 3, 4, 1, 1, 5, 6, 1, 7]
+remove_duplicatas(lista_com_duplicatas)
